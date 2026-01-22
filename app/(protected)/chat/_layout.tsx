b16243fdcd67d402ddb6278523b5c96fab8d1417
yerros <yerisrifan@icloud.com>
2026-01-22 18:52:07 +0700

    Refactor ChatLayout by removing the custom HeaderBackButton component and unnecessary imports, streamlining the layout code.

--- app/(protected)/chat/_layout.tsx
+++ app/(protected)/chat/_layout.tsx
@@ -1,21 +1,9 @@
 import HeaderBackButton from "@/components/HeaderBackButton";
+
+
 import { Stack } from "expo-router";
 
-import { FontAwesome } from "@expo/vector-icons";
-import { router, Stack } from "expo-router";
-import { PressableOpacity } from 'pressto';
-import { View } from "react-native";
 
-//  for back button
-const HeaderBackButton = () => {
-    return (
-        <PressableOpacity onPress={() => router.back()} >
-            <View className="items-center justify-center w-10 h-10">
-                <FontAwesome name='arrow-left' size={24} color='#fff' />
-            </View>
-        </PressableOpacity>
-    )
-}
 export default function ChatLayout() {
     return (
         <Stack screenOptions={{ headerShown: false }} initialRouteName="index">
